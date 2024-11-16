6936c66e4757f7f64a1bd10bd96d0b5de0f312ad
Vyacheslav Lutsenko <vyacheslavlutsenkoml@gmail.com>
2024-11-17 03:27:56 +0500

    Initial commit

---
+++ dist/assets/scripts/script.js
@@ -0,0 +1,10 @@
+// MASK
+const inputsTypeTel = document.querySelectorAll('input[type="tel"]')
+if(inputsTypeTel.length > 0) {
+	inputsTypeTel.forEach((input) => {
+		new Inputmask({
+			mask: '+7 999 999 99 99',
+			placeholder: '+7 XXX XXX XX XX',
+		}).mask(input);
+	})
+}
